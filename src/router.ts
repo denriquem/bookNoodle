8118e9c2e908d21776b6ac64449175f9ef90f01a
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2023-02-08 15:12:32 +0000

    handleInputValidation

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -7,15 +9,37 @@ router.get("/book", (req, res) => {
 	res.json({ message: req.sh_secret });
 });
 router.get("/book/:id", () => {});
-router.put("/book/:id", () => {});
-router.post("/book", () => {});
+router.put(
+	"/book/:id",
+	body("title").isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
+router.post(
+	"/book",
+	body("title").isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 router.delete("/book/:id", () => {});
 
 // Review
 router.get("/review", () => {});
 router.get("/review/:id", () => {});
-router.put("/review/:id", () => {});
-router.post("/review", () => {});
+router.put(
+	"/review/:id",
+	body("title").isString(),
+	body("review").isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
+router.post(
+	"/review",
+	body("title").isString(),
+	body("review").isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 router.delete("/review/:id", () => {});
 
 export default router;
